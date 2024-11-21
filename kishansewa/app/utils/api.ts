04cbc06e0fd83ed45ccbb5627d8b2f78f63ed540
Sujit-Thapa <sujitthapa.professional@gmail.com>
2024-11-21 15:23:49 +0545

    api

--- kishansewa/app/utils/api.ts
+++
@@ -1,6 +0,0 @@
-export async function fetchWeatherData() {
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=YOUR_API_KEY');
-    const data = await response.json();
-    return data;
-  }
-  
